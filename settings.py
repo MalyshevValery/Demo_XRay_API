5d6c911ab3c04f385995ef1d034e79403452ca1c
Malyshev Valery <malyshevalery@gmail.com>
2020-05-19 12:01:41 +0300

    Settings

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
-FRONTEND_URL = 'http://localhost:4200'
+FRONTEND_URL = 'https://image.org.by:22'
 ALLOWED_EXTENSIONS = {'bmp', 'dib', 'jpeg', 'jpg', 'jpe', 'jp2', 'png', 'webp',
                       'pbm', 'pgm', 'ppm', 'sr',
                       'ras', 'tiff', 'dcm',
                       'tif'}
 
-NN_TIMEOUT = 10
+NN_TIMEOUT = 60
 SOCKET_NAME = 'inner_socket'
 UPLOAD_FOLDER = 'upload_folder'
 CONFIG_PATH = 'model/setup_vgg16h_1.json'
